8eee363d5be98a82346876d94a0a69df43908104
Morita Jun <morita.jn0124@gmail.com>
2018-05-27 21:27:15 +0900

    正解の選択肢が無いQuizは投稿不可にした

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,5 @@
 class Choice < ApplicationRecord
   belongs_to :quiz
 
-  validates :quiz_id, presence: true
   validates :sentence, presence: true
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,11 @@
 class Quiz < ApplicationRecord
   has_many :choices
   validates :title, presence: true
+  validate :require_correct_choice
+
+  def require_correct_choice
+    if choices.select{ |choice| choice.correct }.blank?
+      errors.add(:choices, "正解の選択肢がありません。")
+    end
+  end
 end
