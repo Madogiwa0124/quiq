5fadabf77b81bbdb144b5e83435cecb368f73a5e
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-05-30 23:03:04 +0900

    typo修正と登録処理の実装

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,33 @@
+class QuizzesController < ApplicationController
+  def index
+    @quizzes = Quiz.includes(:choices)
+  end
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    prepare_choice
+    if @quiz.save
+      redirect_to quizzes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prepare_choice
+    choices_params[:choices].each { |choice| @quiz.choices.new(choice) }
+  end
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :body, :explanation)
+  end
+
+  def choices_params
+    params.require(:quiz).permit(choices: [:sentence, :correct])
+  end
+end
