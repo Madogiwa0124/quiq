d040228e3ad1a570dd5306cd6fbf1b9c26824e80
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-06-03 15:53:57 +0900

    validate周りを修正

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,9 +3,24 @@ class Quiz < ApplicationRecord
   accepts_nested_attributes_for :choices
 
   validates :title, presence: true
-  validate :require_correct_choice
+  validate :require_choice
+  validate :require_only_correct_choice, unless: -> { valid_choices.length < 2 }
 
-  def require_correct_choice
-    errors.add(:choices, '正解の選択肢がありません。') if choices.select(&:correct).blank?
+  def require_choice
+    errors.add(:choices, '選択肢は、2個以上指定してください') if valid_choices.length < 2
+  end
+
+  def require_only_correct_choice
+    errors.add(:choices, '正解の選択肢は、1つ指定してください') if currect_choices.length != 1
+  end
+
+  private
+
+  def valid_choices
+    choices.select(&:valid?)
+  end
+
+  def currect_choices
+    choices.select(&:correct)
   end
 end
