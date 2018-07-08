7ab832a1e716147f033e4f7665f13b8540da6b79
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-07-08 22:22:54 +0900

    選択肢別の回答数を表示するようにした

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -3,4 +3,9 @@ class Choice < ApplicationRecord
   has_one :answer_choice, dependent: :destroy
 
   validates :sentence, presence: true
+
+  def answers_count
+    answers_count = answer_choice&.answer_count
+    answers_count ? answers_count : 0
+  end
 end
