6e2fc6b21603eeb84a12fce6a6a63539c2eac02a
Morita Jun <morita.jn0124@gmail.com>
2018-07-08 22:22:54 +0900

    選択肢別の回答数を表示するようにした

--- spec/factories/answer_choices.rb
+++ spec/factories/answer_choices.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :answer_choice do
-    
+    association :quiz
+    association :choice
+    answer_count 1
   end
 end
--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     title 'タイトル'
     body '本文'
     explanation '解説'
+
+    trait :with_choices do
+      before(:create) do |quiz|
+        4.times do |i|
+          quiz.choices << FactoryBot.build(:choice, quiz: quiz, sentence: "選択肢_#{i}")
+        end
+        quiz.choices.first.correct = true
+      end
+    end
   end
 end
