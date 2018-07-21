c00fcac197cc27edb4ed506604fa320ed5823b5c
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-07-21 21:05:43 +0900

    Quiz.rankingのテストを追加

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -24,6 +24,29 @@ RSpec.describe Quiz, type: :model do
     end
   end
 
+  describe '.ranking' do
+    let!(:quiz_1) { FactoryBot.create(:quiz, :with_choices) }
+    let!(:quiz_2) { FactoryBot.create(:quiz, :with_choices) }
+    let!(:quiz_3) { FactoryBot.create(:quiz, :with_choices) }
+
+    before do
+      FactoryBot.create(:answer_choice, quiz: quiz_1, choice: quiz_1.choices[0], answer_count: 3)
+      FactoryBot.create(:answer_choice, quiz: quiz_1, choice: quiz_1.choices[1], answer_count: 2)
+      FactoryBot.create(:answer_choice, quiz: quiz_2, choice: quiz_2.choices[0], answer_count: 1)
+      FactoryBot.create(:answer_choice, quiz: quiz_2, choice: quiz_2.choices[1], answer_count: 2)
+      FactoryBot.create(:answer_choice, quiz: quiz_3, choice: quiz_3.choices[1])
+    end
+
+    it 'ランキング順にQuizのインスタンスを取得出来ること' do
+      result = described_class.ranking(2)
+      expect(result[0][:quiz]).to eq quiz_1
+      expect(result[0][:rank]).to eq 1
+      expect(result[0][:count_sum]).to eq 5
+      expect(result[1][:quiz]).to eq quiz_2
+      expect(result[1][:rank]).to eq 2
+    end
+  end
+  
   describe '#correct_choice' do
     let(:quiz) { FactoryBot.build(:quiz) }
     let(:correct_choice) { FactoryBot.build(:choice, quiz: quiz, correct: true) }
